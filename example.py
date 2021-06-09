fe4dda1c5554ee83bc8026e2537db7c38817da90
Paul Orland <orlandpm@users.noreply.github.com>
2021-06-09 09:49:56 -0700

    Create example.py

---
+++ example.py
@@ -0,0 +1,10 @@
+# Here is an example of a Python file uploaded to GitHub
+
+def sign(n):
+    if n > 0:
+        return "positive"
+    elif n == 0:
+        return "negative"
+    else:
+        return "zero"
+    
